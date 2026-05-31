8c9327577aae9c393d0eef6e85109d1c8391e6ab
shreshtharoyy <shreshttharoy16@gmail.com>
2026-05-31 11:42:24 +0530

    Implement answer generation

--- pipeline/generator.py
+++ pipeline/generator.py
@@ -7,11 +7,7 @@ def generate_answer(prompt):
     inputs = tokenizer(prompt, return_tensors="pt")
     outputs = model.generate(**inputs, max_new_tokens=15, do_sample=False, repetition_penalty=1.1)
 
-    # print("Prompt Length:", inputs["input_ids"].shape[1])
-    # print("Output Length:", outputs.shape[1])
-
     generated_tokens = outputs[0][inputs["input_ids"].shape[1]:]
-    # print("Generated Tokens:", generated_tokens)
 
     answer = tokenizer.decode(generated_tokens, skip_special_tokens=True)
     answer = answer.split("Answer:")[-1].strip()
